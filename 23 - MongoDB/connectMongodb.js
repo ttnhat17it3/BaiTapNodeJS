666b31d5531fafa0d8a9e5afa28d23d8f8110602
ttnhat17it3 <38066091+ttnhat17it3@users.noreply.github.com>
2021-07-30 16:48:43 +0700

    Update connectMongodb.js

--- 23 - MongoDB/connectMongodb.js
+++ 23 - MongoDB/connectMongodb.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
-connectDb = async () => {
+connectDB = async () => {
   await mongoose.connect("mongodb://localhost:27017/training", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true,
   });
-  console.log("Connect Success!");
+  console.log("Connect database successfully!");
 };
 
-module.exports = { connectDb };
+module.exports = { connectDB };
